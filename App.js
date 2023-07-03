69db31e6ebc34bb428468df26baa8c87a2f63cb2
Ammar Daham <ammar.daham@metropolia.fi>
2023-07-03 18:14:08 +0300

    Exercise 10.12 done

--- App.js
+++ App.js
@@ -5,17 +5,18 @@ import { ApolloProvider } from '@apollo/client';
 import Main from './src/components/Main';
 
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest.extra);
+
   return (
     <NativeRouter>
-
       <ApolloProvider client={apolloClient}>
         <Main />
-
       </ApolloProvider>
     </NativeRouter>
   );
