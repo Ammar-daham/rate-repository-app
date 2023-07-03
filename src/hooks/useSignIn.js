b04d8074da51305a9a73ddfcdb79b24c55ab4630
Ammar Daham <ammar.daham@metropolia.fi>
2023-07-03 19:26:32 +0300

    Exercise 10.14: storing the access token step1 done

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,14 +1,19 @@
 // hooks/useSignIn.js
 import { useMutation } from '@apollo/client';
 import { AUTHORIZE } from '../graphql/mutations';
+import AuthStorage from '../utils/authStorage'
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHORIZE);
 
+  const authStorage = new AuthStorage();
+
+
   const signIn = async ({ username, password }) => {
     const credentials = { username, password };
     await mutate({ variables: { credentials } });
-    console.log(result.data.authenticate.accessToken)
+    const { accessToken } = result.data.authenticate;
+    authStorage.setAccessToken(accessToken);
   };
 
   return [signIn, result];
